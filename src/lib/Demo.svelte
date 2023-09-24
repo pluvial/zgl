439e63460c6e1bc89692af8b4270cd25056e6256
jpaquim <joaopaquim@gmail.com>
2023-09-24 21:40:03 +0100

    Move styles to Demo.svelte

--- src/lib/Demo.svelte
+++ src/lib/Demo.svelte
@@ -1,6 +1,7 @@
-<script>
+<script lang="ts">
 	import { onMount } from 'svelte';
-	import './demo/style.css';
+
+	import type { DemoApp } from './demo/main.js';
 
 	// import { DemoApp } from './demo/main.js';
 	import { NeuralCA } from './demo/NeuralCA.js';
@@ -25,7 +26,7 @@
 	import { Springs } from './demo/Springs.js';
 	import { ReactionDiffusion } from './demo/ReactionDiffusion.js';
 
-	let app;
+	let app: DemoApp;
 
 	onMount(async () => {
 		const { DemoApp } = await import('./demo/main.js');
@@ -69,7 +70,7 @@
 </script>
 
 <details id="panel" open>
-	<summary><a href="https://github.com/google/swissgl">SwissGL</a> demos</summary>
+	<summary><a href="https://github.com/pluvial/zgl">ZGL</a> demos</summary>
 	<div id="cards" />
 </details>
 <div id="demo">
@@ -84,3 +85,92 @@
 	<a id="sourceLink" href="" target="_blank"><button title="source code">&lt;&gt;</button></a>
 	<button title="fullscreen" on:click={fullscreen}>⛶</button>
 </div>
+
+<style>
+	:global(body) {
+		box-sizing: border-box;
+		background: black;
+		margin: 0px;
+		color: white;
+		overflow: hidden;
+		font-family: 'Roboto Mono', monospace;
+		user-select: none;
+	}
+
+	#panel {
+		width: 200px;
+		position: fixed;
+		background: rgba(0, 0, 0, 0.5);
+	}
+
+	#panel summary {
+		padding: 8px;
+	}
+
+	#cards {
+		overflow: auto;
+		height: 95vh;
+	}
+
+	:global(.card) {
+		padding: 4px;
+		margin: 8px;
+		border: 1px solid grey;
+		border-radius: 5px;
+		font-size: 14px;
+	}
+
+	#panel :global(img) {
+		max-width: 100%;
+	}
+
+	#demo {
+		width: 100%;
+		height: 100vh;
+	}
+
+	#c {
+		width: 100%;
+		height: 100%;
+		background: black;
+		touch-action: none;
+	}
+
+	:global(#gui) {
+		position: fixed;
+		bottom: 0px;
+		right: 50px;
+	}
+
+	#buttons {
+		position: fixed;
+		bottom: 10px;
+		right: 10px;
+	}
+
+	button {
+		appearance: none;
+		width: 48px;
+		height: 40px;
+		margin: 8px;
+		font-size: 24px;
+		background-color: rgba(0, 0, 0, 0.5);
+		color: white;
+		border: none;
+		text-align: center;
+		text-decoration: none;
+		display: block;
+	}
+
+	button:hover {
+		background-color: rgba(80, 80, 80, 0.8);
+	}
+
+	#buttons a {
+		text-decoration: none;
+	}
+
+	a {
+		color: aquamarine;
+	}
+</style>
