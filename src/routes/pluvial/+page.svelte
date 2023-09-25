5c0a5f6da717726136e985b88a72f9b50902d5fe
jpaquim <joaopaquim@gmail.com>
2023-09-25 22:44:09 +0100

    Spiral logo for /pluvial

---
+++ src/routes/pluvial/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import zgl from '$lib/index.js';
+
+	let canvas: HTMLCanvasElement;
+
+	onMount(() => {
+		const z = zgl(canvas);
+		z.loop(({ time }) => {
+			z.adjustCanvas();
+			z({
+				time,
+				Aspect: 'cover',
+				FP: `
+            sin(length(XY)*vec3(1,2,3)*7.
+            +atan(XY.x,XY.y)*3.),1`
+			});
+		});
+	});
+</script>
+
+<canvas bind:this={canvas} />
+
+<style>
+	:global(body, html) {
+		margin: 0;
+		padding: 0;
+		overflow: hidden;
+	}
+
+	canvas {
+		width: 100vw;
+		height: 100vh;
+	}
+</style>
