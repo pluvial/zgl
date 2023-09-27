48334f7b7e985739c00f73da3eee797a4f823aa8
jpaquim <joaopaquim@gmail.com>
2023-09-27 17:25:21 +0100

    Add bit field example

---
+++ src/routes/(full)/bit-field/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { Loop } from '$lib/index.js';
+
+	let k = 9;
+</script>
+
+<Loop
+	f={({ z, time }) => {
+		z.adjustCanvas(1);
+
+		z({ t: time, k, FP: `1-((I.x+int(t*40.))/4^(I.y+int(t*20.))/4)%int(k)` });
+	}}
+></Loop>
