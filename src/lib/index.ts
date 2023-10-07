f8739dc813b313470160a1824ae9a28edd686825
jpaquim <joaopaquim@gmail.com>
2023-10-07 13:54:38 +0100

    pnpm create vite --template vanilla-ts

--- src/lib/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './swissgl.js';
-export { SwissGL as default } from './swissgl.js';
--- src/routes/[name]/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import { page } from '$app/stores';
-	import demos, { pathToName } from '../../demos/index.svelte.js';
-
-	$: Demo = demos[pathToName[$page.params.name]];
-</script>
-
-<svelte:component this={Demo} />
