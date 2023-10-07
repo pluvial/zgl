dce79027fb6bc1c1f1bc4a2efdbb0dbdabf289e5
jpaquim <joaopaquim@gmail.com>
2023-10-07 13:55:42 +0100

    Add zgl lib as renamed version of swissgl

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,15 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import zgl from './zgl.js';
+import './style.css';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const z = zgl(document.querySelector('canvas')!);
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+z.loop(({ time }) => {
+  z.adjustCanvas();
+  z({
+    time,
+    Aspect: 'cover',
+    FP: `
+sin(length(XY)*vec3(30,30.5,31)
+-time+atan(XY.x,XY.y)*3.),1`,
+  });
+});
