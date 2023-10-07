44be15fc79b3058667f61cb8dff8e9605eba0383
jpaquim <joaopaquim@gmail.com>
2023-10-07 18:15:49 +0100

    Minify using terser when process.env.TERSER is set

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,13 @@ import glsl from 'vite-plugin-glsl';
 import { viteSingleFile } from 'vite-plugin-singlefile';
 import htmlMinimize from '@sergeymakinen/vite-plugin-html-minimize';
 
+const minify = process.env.TERSER ? 'terser' : 'esbuild';
+
 export default defineConfig({
   plugins: [
     glsl({ compress: true }),
     viteSingleFile({ removeViteModuleLoader: true }),
     htmlMinimize.default(),
   ],
-  build: { target: 'esnext' },
+  build: { target: 'esnext', minify },
 });
