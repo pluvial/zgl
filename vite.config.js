efb9065e64a3063b029644d5f5bd3c6766a46062
jpaquim <joaopaquim@gmail.com>
2023-10-07 16:22:30 +0100

    Inline js and css into html

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,7 @@
 import { defineConfig } from 'vite';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 
-export default defineConfig({ build: { target: 'esnext' } });
+export default defineConfig({
+  plugins: [viteSingleFile()],
+  build: { target: 'esnext' },
+});
