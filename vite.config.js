144b2ec3059cdd794b4c279ffa6f824e74f57769
jpaquim <joaopaquim@gmail.com>
2023-10-07 16:34:04 +0100

    Compress glsl templates

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
+import glsl from 'vite-plugin-glsl';
 import { viteSingleFile } from 'vite-plugin-singlefile';
 
 export default defineConfig({
-  plugins: [viteSingleFile()],
+  plugins: [glsl({ compress: true }), viteSingleFile()],
   build: { target: 'esnext' },
 });
