b28a2cebb04b56e2d6e590fb43200d7ac0619735
jpaquim <joaopaquim@gmail.com>
2023-11-03 02:29:18 +0000

    Add vite config to build zgl as library

--- vite.lib.config.js
+++ vite.lib.config.js
@@ -1,18 +1,13 @@
 import { defineConfig } from 'vite';
 import glsl from 'vite-plugin-glsl';
-import { viteSingleFile } from 'vite-plugin-singlefile';
-import htmlMinimize from '@sergeymakinen/vite-plugin-html-minimize';
 
 const { MINIFY } = process.env;
 const minify = ['esbuild', 'terser'].includes(MINIFY) ? MINIFY : false;
 
 export default defineConfig({
-  plugins: [
-    glsl({ compress: true }),
-    viteSingleFile({ removeViteModuleLoader: true }),
-    htmlMinimize.default(),
-  ],
+  plugins: [glsl({ compress: true })],
   build: {
+    lib: { entry: 'src/zgl.ts', formats: ['es'] },
     target: 'esnext',
     minify,
     terserOptions: minify === 'terser' ? { format: { comments: false } } : null,
